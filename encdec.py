c3061355431758bf10b07ba8e8c6b4832bf5019b
gerow <gerow.mike@gmail.com>
2013-02-22 04:10:21 -0800

    Added more documentation to Monoalphabetic cipher

--- encdec.py
+++ encdec.py
@@ -53,36 +53,61 @@ class EncryptorDecryptor:
     raise NotImplementedError("An EncryptorDecryptor must implement this")
 
 class Monoalphabetic(EncryptorDecryptor):
+  """
+  Implemtation of EncryptorDecryptor that provides monoalphabetic ciphers
+  """
   def __init__(self):
+    """
+    Init only initializes an empty key and empty reverse key
+    """
     self.__key = {}
     self.__inv_key = {}
 
   def __rand_pop(self, elements):
+    """
+    Randomly pop an element from elements and return it
+    """
     random.shuffle(elements)
     return elements.pop(0)
 
   def __generate_inv_key(self):
+    """
+    Generate the inverse key so that we can decrypt
+    """
     for key, value in self.__key.iteritems():
       self.__inv_key[value] = key
 
   def set_key(self, key):
+    """
+    Set our key to the key given.  This method expects a dict
+    that maps all the availible symbols to some other availible symbol
+    """
     self.__key = copy.copy(key)
     self.__inv_key = {}
     self.__generate_inv_key()
 
   def encrypt(self, plaintext):
+    """
+    Encrypt the given plaintext using the key we have stored
+    """
     out = ""
     for c in plaintext:
       out += self.__key[c]
     return out
 
   def decrypt(self, ciphertext):
+    """
+    Decrypt the plaintext using the inv_key we have stored
+    """
     out = ""
     for c in ciphertext:
       out += self.__inv_key[c]
     return out
     
   def generate_key(self, args):
+    """
+    Generate a random key
+    """
     # We're going to ignore the args since
     # we don't need them
     symbols = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
@@ -95,10 +120,16 @@ class Monoalphabetic(EncryptorDecryptor):
     return key
 
   def dump_key(self, filename):
+    """
+    Write our dict to the file given using pickle
+    """
     with open(filename, "w") as f:
       pickle.dump(self.__key, f)
 
   def load_key(self, filename):
+    """
+    Read our dict from the file provided using pickle
+    """
     with open(filename, "r") as f:
       self.__key = pickle.load(f)
     self.__generate_inv_key()
