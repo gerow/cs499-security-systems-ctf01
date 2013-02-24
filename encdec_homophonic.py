56014259bba40a6972b24fff6a340e1b5875fa82
gerow <gerow.mike@gmail.com>
2013-02-24 15:16:26 -0800

    Adding Jerry's implementation of homophonic and stream ciphers

---
+++ encdec_homophonic.py
@@ -0,0 +1,72 @@
+import random
+import util
+import key
+import pickle
+from encdec import*
+
+class HomophonicAlphabet(EncryptorDecryptor):
+     """
+     Implemtation of EncryptorDecryptor that provides homophonic ciphers
+     @author jiachenz
+     """
+def __init__(self):
+     """
+     Init empty keys
+     """
+	 self.__key = {}
+	 self.__inv_key ={}
+	 
+def __generate_inv_key(self):
+     """
+     Generate the inverse key to decrypt
+     """
+     inv_key = {}
+     for k,v in self.__key.iteritems():
+            for num in v:
+                inv_key[num] = k
+
+     return inv_key
+	
+def set_key(self key):
+	self.__key = copy.copy(key)
+	self.__inv_key = {}
+	self.__generate_inv_key()
+
+def encrypt(self, plaintext):
+	out = ""
+	keys = copy.copy(self.__key)
+	for c in plaintext:
+		out += str(keys[c].pop())
+	return out
+	
+def decrypt(self, ciphertext):
+	out = ""
+	
+	
+def generate_key(self, args): 
+	symbols = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+               "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
+               "y", "z", ".", ",", "!", "?", " "]
+	ciphers = {}
+	for num in range (100,1000):
+		ciphers.append(num)
+        symbols_availible = copy.copy(symbols) 
+        key = {}
+	occurrences = {}	
+	for symbol in symbols:
+		occurrences[symbol] = plaintext.count(symbol)
+		for i in range(0, occurrences[symbol])
+		key[symbol].append(util.rand_pop(ciphers))
+		
+	return key
+
+
+def dump_key(self, filename):
+	with open(filename, "w") as f:
+            pickle.dump(self.__key, f)
+	
+def load_key(self, filename):
+	with open(filename, "r") as f:
+          self.__key = pickle.load(f)
+        self.__generate_inv_key()
+
