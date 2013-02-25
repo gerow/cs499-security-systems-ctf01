1cba4414e44c9353622f1cc2351f123204446316
gerow <gerow.mike@gmail.com>
2013-02-24 21:42:38 -0800

    Added an analysis method to detect odd spaces
    Haven't tested it quite yet

--- encrack_analysis.py
+++ encrack_analysis.py
@@ -24,3 +24,41 @@ class Analysis:
     for k,v in out.iteritems():
       out[k] = float(v)/float(total_len)
     return out
+
+  def detect_odd_space(self, messages):
+    """
+    Return a list of indicies for each
+    message if we detect odd spaces.  That
+    includes spaces at the beginning or end
+    of the message or multiple spaces next to
+    each other. In the case of multiple spaces
+    next to each other we list the index of the
+    first instance unless we have three spaces
+    in which case we list the first two and so on
+    """
+    problems = []
+    for i, message in enumerate(messages):
+      # Create a new entry for this message
+      problems.append([])
+      # Ignore message if empty
+      if not message:
+        continue
+      if message[0] == " ":
+        problems[i].append(0)
+      for j, c in enumerate(message):
+        try:
+          if c == " " and message[j + 1] == " ":
+            problems[i].append(j)
+        except IndexError:
+          # Just ignore it, we're cool
+          pass
+      if message[-1] == " ":
+        problems[i].append(len(message) - 1)
+    # Return an empty list if it is nothing but
+    # empty list (so this can be used with an if
+    # statement)
+
+    for message_problem in problems:
+      if message_problem:
+        return problems
+    return []
