76a297f090f3c49d514dee24507e3360eda26d17
rpuncel <puncel@usc.edu>
2013-02-28 21:34:10 -0800

    sniffer prints out file with tcp data

--- sniffer.py
+++ sniffer.py
@@ -4,7 +4,10 @@ import cracker
 import thread
 #from scapy.all import IP, TCP
 import socket
+import time
 from dpkt import ip, tcp, hexdump
+
+
 """ Module that will lift messages off the network
 for decryption
 """
@@ -19,24 +22,25 @@ def crack():
 	while 1:
 		pass
 """
+
+count = 0
 def crack(dummy, payload):
+	global count
 	print "Got packet"
-	print payload
 	data = payload.get_data()
 	packet = ip.IP(data)
-	print packet.tcp.data
+	#print packet.tcp.data
 	#print payload[TCP]
+	mData = packet.tcp.data
+	if len(mData) != 0:
+		outFileName = str(packet.tcp.dport) + "_id" + str(count)
+		fout = open(outFileName, 'wb')
+		print mData
+		fout.write(mData)
+		fout.close()
+		count = count + 1
 	payload.set_verdict(nfqueue.NF_ACCEPT)
-	"""
-	try:
-		thread.start_new_thread(cracker.monoAlphabeticCrack, ())
-		thread.start_new_thread(cracker.polyAlphabeticCrack, ())
-		thread.start_new_thread(cracker.polygramCrack, ())
-		thread.start_new_thread(cracker.homophoneCrack, ())
-		thread.start_new_thread(cracker.streamCrack, ())
-	except:
-		print "Problem with threading in sniffer.crack()
-	"""
+	
 
 if __name__ == "__main__":
 	q = None
